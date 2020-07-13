188054838e89803096391ca400383b731f98d33b
kvbc <64214044+kvbc@users.noreply.github.com>
2020-07-13 13:01:30 +0200

    Update game.h

--- game.h
+++ game.h
@@ -35,7 +35,7 @@ private:
 void Game::exit() {
 	std::cout << '\n';
 	std::cout << m_p1 << "'s wins: " << m_p1.wins << '\n';
-	std::cout << m_p2 << "'s wins: " << m_p2.wins << '\n';
+	std::cout << m_p2 << "'s wins: " << m_p2.wins << "\n\n";
 	system("pause");
 }
 
